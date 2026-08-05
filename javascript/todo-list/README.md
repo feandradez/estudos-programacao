37220f2a792ddc34b804a3f5ada09b77904c9dfe
Fernando Andrade <andradefernando578@gmail.com>
2026-08-04 22:09:19 -0300

    feat: adicionar mini projeto to-do list

---
+++ javascript/todo-list/README.md
@@ -0,0 +1,13 @@
+# To-Do List
+
+Mini projeto de estudo desenvolvido para praticar HTML, CSS e JavaScript puro.
+
+## Tecnologias
+- HTML
+- CSS
+- JavaScript
+
+## Funcionalidades
+- Adicionar tarefas digitando e pressionando Enter ou clicando no botão
+- Marcar tarefa como concluída
+- Deletar tarefa
---
+++ javascript/todo-list/script.js
@@ -0,0 +1,56 @@
+// Selecionando os elementos do HTML.
+const campo = document.querySelector("#campo")
+const botao = document.querySelector("#botao")
+const lista = document.querySelector("#lista")
+
+// Array vazio para receber cada task.
+const tarefas = []
+
+/* Função renderizar para atualizar sempre que tiver mudanças no array e retornar um <li> com o botao de excluir e a função de riscar o texto quando concluída. */
+function renderizar() {
+    lista.innerHTML = tarefas.map((task, index) => {
+        return `<li onclick="concluir(${index})" style="${task.concluida ? 'text-decoration: line-through' : ''}"> ${task.descricao} <button onclick="excluir(${index})">Excluir</button>
+    </li>`
+    }).join("")
+}
+
+// Função pra adicionar as tasks ao array vazio.
+function adicionar() {
+    
+    const task = {
+        descricao: campo.value,
+        concluida: false
+    }
+
+    tarefas.push(task);
+    renderizar();
+
+}
+// Função para excluir a task do array.
+function excluir(index) {
+    tarefas.splice(index, 1);
+    renderizar();
+}
+
+// Função para concluir a task.
+function concluir(index) {
+    tarefas[index].concluida = !tarefas[index].concluida;
+    renderizar()
+}
+
+// EventListener para o "Enter".
+campo.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        adicionar();
+    }
+})
+
+// EventListener para o "Click" no botão.
+botao.addEventListener("click", () => {
+    adicionar();
+})
+
+// Alternar o tema
+document.querySelector("#tema").addEventListener("click", () => {
+    document.body.classList.toggle("claro")
+})
