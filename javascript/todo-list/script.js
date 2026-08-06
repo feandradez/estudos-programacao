821eedbc1169824d10a0f45d0f297e0a3e68b495
Fernando Andrade <andradefernando578@gmail.com>
2026-08-06 20:53:38 -0300

    feat: add filter and task counter

--- javascript/todo-list/script.js
+++ javascript/todo-list/script.js
@@ -6,18 +6,31 @@ const lista = document.querySelector("#lista")
 // Array vazio para receber cada task.
 const tarefas = []
 
+// Filtro de tarefas -> variavel com filtro ativo.
+let filtroAtivo = "todas"
+
 /* Função renderizar para atualizar sempre que tiver mudanças no array e retornar um <li> com o botao de excluir e a função de riscar o texto quando concluída. */
 function renderizar() {
+
+    let tarefasFiltradas = tarefas
+
+    if (filtroAtivo === "pendentes") {
+        tarefasFiltradas = tarefas.filter(task => task.concluida === false)
+    }
+    else if (filtroAtivo === "concluidas") {
+        tarefasFiltradas = tarefas.filter(task => task.concluida === true)
+    }
 
     if(tarefas.length === 0) {
         lista.innerHTML = "Nenhuma tarefa adicionada ainda."
     } else {
-        lista.innerHTML = tarefas.map((task, index) => {
+        lista.innerHTML = tarefasFiltradas.map((task, index) => {
         return `<li onclick="concluir(${index})" style="${task. concluida ? 'text-decoration: line-through' : ''}"> ${task.descricao} <button onclick="event.stopPropagation(); excluir(${index})">Excluir</button>
         </li>`
     }).join("")
     }  
 
+    atualizarContador()
 }
 
 // Função pra adicionar as tasks ao array vazio.
@@ -48,6 +61,20 @@ function concluir(index) {
     renderizar()
 }
 
+// Função para atualizar o contador.
+function atualizarContador() {
+    const totalTarefas = tarefas.length;
+    const concluidas = tarefas.filter(task => task.concluida === true).length;
+    const pendentes = tarefas.filter(task => task.concluida === false).length;
+
+    // Usando innerHTML pra mostrar na tela as infos.
+    document.querySelector("#total").innerHTML = `Total: ${totalTarefas}`
+
+    document.querySelector("#concluidas").innerHTML = `Concluidas: ${concluidas}`
+
+    document.querySelector("#pendentes").innerHTML = `Pendentes: ${pendentes}`
+}
+
 // EventListener para o "Enter".
 campo.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
@@ -63,6 +90,21 @@ botao.addEventListener("click", () => {
 // Alternar o tema
 document.querySelector("#tema").addEventListener("click", () => {
     document.body.classList.toggle("claro")
+})
+
+
+// EventListener nos botoes para os filtros.
+document.querySelector("#filtro-todas").addEventListener("click", () => {
+    filtroAtivo = "todas"
+    renderizar()
+})
+document.querySelector("#filtro-pendentes").addEventListener("click", () => {
+    filtroAtivo = "pendentes"
+    renderizar()
+})
+document.querySelector("#filtro-concluidas").addEventListener("click", () => {
+    filtroAtivo = "concluidas"
+    renderizar()
 })
 
 renderizar()
